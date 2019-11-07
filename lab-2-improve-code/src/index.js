4686ca2034b8a47e4121576e29af6876328b1d21
Alfie Binnie <alfiebinnie@gmail.com>
2019-11-07 13:42:34 +0000

    More ESLint madness

--- lab-2-improve-code/src/index.js
+++ lab-2-improve-code/src/index.js
@@ -4,10 +4,8 @@ module.exports = function greeting(i = 'my friend') {
     i = [i];
   }
 
-  const out = 'Hello, ';
-
   let nStr = '';
-  for (const j in i) { nStr = nStr + i[j] + ((parseInt(j) + 2 === i.length) ? ', and ' : ((parseInt(j) + 1 === i.length) ? '' : ', ')); }
+  for (const j in i) { nStr = nStr + i[j] + ((parseInt(j, 10) + 2 === i.length) ? ', and ' : ((parseInt(j, 10) + 1 === i.length) ? '' : ', ')); }
 
   if (nStr.toUpperCase() === nStr) return `HELLO ${nStr}!`;
 
